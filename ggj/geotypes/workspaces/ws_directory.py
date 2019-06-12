8022786e26a19112cf2c3e0a0fc203bab1710ce3
Byezy <dbye68@gmail.com>
2019-06-12 13:39:28 +1000

    dev update

--- ggj/geotypes/workspaces/ws_directory.py
+++ ggj/geotypes/workspaces/ws_directory.py
@@ -1,9 +1,18 @@
 from .driver import WorkspaceDriver, SupportedObject, SupportedFormat
 from ...utils.filesystem import is_directory, create_directory
+from collections import OrderedDict
 
 
-class WorkspaceObject:
-    pass
+class WorkspaceObject(SupportedObject):
+    def __init__(self, *args, **kwargs):
+        super(SupportedObject, self).__init__(*args, **kwargs)
+        self.formats = [SupportedFormat("Comma Separated Values", "CSV", ".csv"),
+                        SupportedFormat("dBase", "dBase", ".dbf")]
+
+    def search(self, workspace):
+        for format in self.formats:
+            print(format.extension)
+        return []
 
 
 class TableObject(SupportedObject):
@@ -55,7 +64,11 @@ class DirectoryDriver(WorkspaceDriver):
         return is_directory(workspace)
 
     def searchable_objects(self):
-        return {'tables': TableObject(), 'rasters': RasterObject(), 'vectors': VectorObject(), 'other': OtherObject()}
+        return OrderedDict((k, v) for k, v in [['workspaces', WorkspaceObject()],
+                                               ['tables', TableObject()],
+                                               ['rasters', RasterObject()],
+                                               ['vectors', VectorObject()],
+                                               ['other', OtherObject()]])
 
     def create(self, **kwargs):
         d = create_directory(kwargs['parent'], kwargs['name'])
